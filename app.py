939ed798f77a78c172bcbb8039232b99f4ac0815
Néstor Andrés <nestor2509@gmail.com>
2019-07-22 17:10:24 -0500

    modelo terreno

--- app.py
+++ app.py
@@ -12,19 +12,21 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
-@app.route("/take_canvas", methods=["POST"])
-def disp_pic():
-    data = request.form['img']
-    encoded_data = data.split(",")[1]
-    nparr = np.fromstring(encoded_data.decode('base64'), np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    cv2.imwrite("output_render/render_canvas.jpg", img)
-    subprocess.call("python pix2pix-tensorflow/pix2pix.py --mode test", shell=True)
-    # cv2.imwrite("render_canvas.jpg", img)
-    # cv2.imshow('', img)
-    # cv2.waitKey(1)
-    # cv2.destroyAllWindows()
-    return json.dumps({'status': encoded_data})
+# @app.route("/take_canvas", methods=["POST"])
+# def disp_pic():
+#     data = request.form['img']
+#     encoded_data = data.split(",")[1]
+#     # nparr = np.fromstring(encoded_data.decode('base64'), np.uint8)
+#     nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+#     img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+#     cv2.imwrite("output_render/render_canvas.jpg", img)
+#     # subprocess.call("python pix2pix-tensorflow/pix2pix.py --mode test --input_dir output_render --output_dir render --checkpoint terrain-model", shell=True)
+#     subprocess.call("python pix2pix-tensorflow/pix2pix.py --mode", shell=True)
+#     # cv2.imwrite("render_canvas.jpg", img)
+#     # cv2.imshow('', img)
+#     # cv2.waitKey(1)
+#     # cv2.destroyAllWindows()
+#     return json.dumps({'status': encoded_data})
 
 if __name__ == "__main__":
     app.run()
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,19 @@
 var scketch = function(p) {
+
     var color = p.color(239,223,193);
-    var loaded = false;
+    var isTransfering = false;
+    var modelReady = false;
+    var pix2pix;
 
     p.setup = function() {
-        p.createCanvas(500,500);
+        p.createCanvas(256,256);
         p.background(142,181,219);
+
+        pix2pix = ml5.pix2pix('/static/terrenos.pict', function(){
+            console.log('modelo cargado');
+            modelReady = true;
+            p.transfer();
+        });
     }
 
     p.draw = function() {
@@ -13,38 +22,58 @@ var scketch = function(p) {
     }
         
     p.mouseReleased = function() {
-        if(!loaded){
-            var canvas = document.getElementById('defaultCanvas0');
-            var imgURL = canvas.toDataURL();
-            loaded = true;
-
-            $.ajax({
-                type: "POST",
-                url: "/take_canvas",
-                data: {img: imgURL},
-                success: function(data) {
-                    // console.log(data);
-                    loaded = false;
-                },
-                error: function(data) {
-                }
-            }).done(function() {
-                // console.log("enviado");
-            });
-        }
-        
+        if(modelReady && !isTransfering) p.transfer();
     }
 
     p.mouseDragged = function() {
         // p.ellipse(p.mouseX, p.mouseY, 20, 20);
-        p.strokeWeight(20);
+        p.strokeWeight(30);
         p.line(p.pmouseX, p.pmouseY, p.mouseX, p.mouseY);
     }
 
     p.changeColor = function(color_temp){
         color = p.color(color_temp[0],color_temp[1],color_temp[2]);
     }
+
+    p.transfer = function() {
+        isTransfering = true;
+
+        const canvas = p.select('canvas').elt;
+
+        pix2pix.transfer(canvas, function(err, result){
+            if(err) console.log(err);
+
+            isTransfering = false;
+            $('#output').empty();
+            p.createImg(result.src).class('resultado').parent('output');
+        });
+    }
 }
+
+function transfer() {
+    // Set isTransfering to true
+    isTransfering = true;
+  
+    // Select canvas DOM element
+    const canvasElement = select('canvas').elt;
+  
+    // Apply pix2pix transformation
+    pix2pix.transfer(canvasElement, function(err, result) {
+      if (err) {
+        console.log(err);
+      }
+      if (result && result.src) {
+        // Set isTransfering back to false
+        isTransfering = false;
+        // Clear output container
+        outputContainer.html('');
+        // Create an image based result
+        createImg(result.src).class('border-box').parent('output');
+        // Show 'Done!' message
+        statusMsg.html('Done!');
+      }
+    });
+  }
 
 var canvas_draw = new p5(scketch, 'canvas_container');
 
@@ -59,6 +88,10 @@ var canvas_draw = new p5(scketch, 'canvas_container');
             var color = $(this).attr('data-color').split(',');
             canvas_draw.changeColor(color);
         });
+
+
+        console.log('ml5 version:', ml5.version);
+
 	});
 	
 })(jQuery, this);
